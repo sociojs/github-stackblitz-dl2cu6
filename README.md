9a8a7374e8442ae0b0563b574acbe253ecee665d
t-bee <gojavapro@gmail.com>
2022-05-22 16:28:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-dl2cu6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-dl2cu6)
---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLink href="/">Home</NuxtLink> | <NuxtLink href="/test">Test</NuxtLink>
+    <p>Hello {{ world }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      world: 'Test Page !!!!',
+    };
+  },
+};
+</script>
